5c687c7f408e33f35760617b22d236fd423e338b
Redrica <de.spring@gmail.com>
2018-02-17 11:01:02 +0300

    Модальное окно заработало

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,18 +1,10 @@
-var promoGoodsButton = document.querySelectorAll('.promo-goods__button');
-var goodsCart = document.querySelectorAll('.goods__cart');
+var promoGoodsButton = document.querySelector('.promo-goods__button');
 var modal = document.querySelector('.modal');
 var modalOff = document.querySelector('.modal__overlay');
 
 promoGoodsButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   modal.classList.remove('modal--closed');
-});
-
-Array.prototype.slice.call(goodsCart).forEach(function(item) {
-  item.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    modal.classList.remove('modal--closed');
-  });
 });
 
 modalOff.addEventListener('click', function () {
